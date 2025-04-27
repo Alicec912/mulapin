4670582d4548125f56a0c1bb468ad6cc4d988754
Alicec912 <alicechen9121@gmail.com>
2025-04-28 11:01:11 +1200

    Update and rename main.py to Building_Blocks_Processing.py

--- Building_Blocks_Processing.py
+++ Building_Blocks_Processing.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 from nltk.tokenize import word_tokenize
 import re
 import pandas as pd
